0f945342fd8c164686e48db60ee56ebe898a1a8c
Suraj Verma <surajverma@rentalcars.com>
2021-07-03 11:45:31 +0100

    styled header component

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ import SearchForm from "./components/SearchForm";
 
 function App() {
   return (
-    <div className="border border-danger">
-      <Header />
+    <div className="">
+      <Header
+        title="Weather Dashboard"
+        subtitle="Weather or not here it comes..."
+      />
       <div className="row border main g-0">
         <div className="border col-sm-12 col-md-3">
           <RecentSearches />
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,10 @@
 const Header = (props) => {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header text-center p-3">
+      <h1>{props.title}</h1>
+      <h2>{props.subtitle}</h2>
+    </div>
+  );
 };
 
 export default Header;
