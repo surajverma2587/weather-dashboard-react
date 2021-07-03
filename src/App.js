a6ba4f5b65868a0a7037fa5890f90884468dc804
Suraj Verma <surajverma@rentalcars.com>
2021-07-03 12:24:13 +0100

    styled search form with logic

--- src/App.js
+++ src/App.js
@@ -1,27 +1,60 @@
+import { Component } from "react";
 import "./App.css";
 import CurrentWeather from "./components/CurrentWeather";
 import Header from "./components/Header";
 import RecentSearches from "./components/RecentSearches";
 import SearchForm from "./components/SearchForm";
 
-function App() {
-  return (
-    <div className="">
-      <Header
-        title="Weather Dashboard"
-        subtitle="Weather or not here it comes..."
-      />
-      <div className="row border main g-0">
-        <div className="border col-sm-12 col-md-3">
-          <RecentSearches />
-        </div>
-        <div className="border col-sm-12 col-md-9">
-          <SearchForm />
-          <CurrentWeather />
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cityName: "Birmingham",
+      fetchData: false,
+    };
+  }
+
+  onCitySearch = (event) => {
+    event.preventDefault();
+    this.setState({
+      fetchData: true,
+    });
+  };
+
+  onChange = (event) => {
+    this.setState({
+      cityName: event.target.value,
+    });
+  };
+
+  shouldComponentUpdate(_, nextState) {
+    return nextState.fetchData;
+  }
+
+  render() {
+    return (
+      <div className="">
+        <Header
+          title="Weather Dashboard"
+          subtitle="Weather or not here it comes..."
+        />
+        <div className="row border main g-0">
+          <div className="border col-sm-12 col-md-3">
+            <RecentSearches />
+          </div>
+          <div className="border col-sm-12 col-md-9">
+            <SearchForm
+              placeholder="Enter city name"
+              onSubmit={this.onCitySearch}
+              onChange={this.onChange}
+            />
+            <CurrentWeather />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,5 +1,26 @@
 const SearchForm = (props) => {
-  return <div className="border search">Search</div>;
+  return (
+    <div className="search">
+      <form onSubmit={props.onSubmit}>
+        <div className="row h-100 search-form">
+          <div className="col-sm-12 col-md-10 p-2">
+            <input
+              type="text"
+              className="form-control"
+              id="search-city"
+              placeholder={props.placeholder}
+              onChange={props.onChange}
+            />
+          </div>
+          <div className="col-sm-12 col-md-2 p-2">
+            <button type="submit" className="btn btn-primary">
+              Search
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default SearchForm;
