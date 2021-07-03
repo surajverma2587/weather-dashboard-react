e4d83ebdc6869513bbfc8805dfba4e2df020f252
Suraj Verma <surajverma@rentalcars.com>
2021-07-03 11:20:30 +0100

    add page layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="border border-danger">
+      <div className="header">Header</div>
+      <div className="row border main g-0">
+        <div className="border col-sm-12 col-md-3">
+          <div>Recent Cities</div>
+        </div>
+        <div className="border col-sm-12 col-md-9">
+          <div className="border search">Search</div>
+          <div className="border weather">Weather Data</div>
+        </div>
+      </div>
     </div>
   );
 }
